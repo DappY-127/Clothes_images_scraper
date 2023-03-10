68691ce9c92f89b493b56f70186ce7d057f59a93
Paul <paul.mshtk.707@gmail.com>
2023-03-10 21:18:35 +0200

    update main.py, create ClothingItem class

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 from selenium import webdriver
 import wget
+
+class ClothingItem:
+    def __init__(self, url):
+        self.url = url
+        self.name = None
+        self.price = None
+        self.id = None
+        self.image_links = []
+        self.image_names = []
+        self.directory = None
+
+if __name__ == '__main__':
+    url = 'plug'    
+    item = ClothingItem(url)    
