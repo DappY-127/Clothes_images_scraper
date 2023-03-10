81463a569f1e2006a8fea8409f170db954aafa43
Paul <paul.mshtk.707@gmail.com>
2023-03-10 21:24:17 +0200

    update main.py, create methods for image downloads

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import os
 import wget
 
 class ClothingItem:
@@ -10,6 +11,18 @@ class ClothingItem:
         self.image_links = []
         self.image_names = []
         self.directory = None
+
+    def create_directory(self):
+        path = os.getcwd()
+        self.directory = os.path.join(path, f'Reserved\ClothesV2\{self.name}({self.id})')
+        os.mkdir(self.directory)  
+
+    def download_images(self):
+        counter = 0
+        for link in self.image_links:
+            save_as = os.path.join(self.directory, f'{self.image_names[counter]}.jpg')
+            wget.download(link, save_as)
+            counter += 1    
 
 if __name__ == '__main__':
     url = 'plug'    
