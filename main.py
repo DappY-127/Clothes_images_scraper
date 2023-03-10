d835f5f0b547b9dbbc34ccd5912b82501317b3a7
Paul <paul.mshtk.707@gmail.com>
2023-03-10 21:26:40 +0200

    update main.py, create extract_data method

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 import os
 import wget
 
@@ -11,6 +14,25 @@ class ClothingItem:
         self.image_links = []
         self.image_names = []
         self.directory = None
+
+    def extract_data(self):
+        driver = webdriver.Chrome(executable_path='drivers\chromedriver.exe')
+        driver.maximize_window()
+        driver.get(self.url)
+        driver_wait = WebDriverWait(driver, 10)
+
+        clothes_name_locator = '[data-selen="product-name"]'
+        clothes_price_locator = '.basic-pricestyled__StyledBasicPrice-ptbrpf-0'
+        clothes_id_locator = '[data-selen="sku"]'
+        clothes_pic_locator = '.thumbnail__Thumbnail-cghdsl-0 img'
+
+        self.name = driver_wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, clothes_name_locator))).text
+        self.price = driver_wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, clothes_price_locator)))
+        self.id = driver_wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, clothes_id_locator))).text
+        clothes_pics = driver.find_elements(By.CSS_SELECTOR, clothes_pic_locator)
+        self.image_links = [clothes.get_attribute('src') for clothes in clothes_pics]
+        self.image_names = [clothes.get_attribute('alt') for clothes in clothes_pics]
+        self.image_links = self.prepare_link(self.image_links)    
 
     def create_directory(self):
         path = os.getcwd()
@@ -26,4 +48,8 @@ class ClothingItem:
 
 if __name__ == '__main__':
     url = 'plug'    
-    item = ClothingItem(url)    
+    item = ClothingItem(url)   
+    item.extract_data()
+    item.create_directory()
+    item.download_images()
+ 
