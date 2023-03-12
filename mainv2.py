d2bd84cacb00fb58b2da5a23f2da22ba927a71d0
Paul <paul.mshtk.707@gmail.com>
2023-03-12 14:20:55 +0200

    update mainv2.py, create ClothingItem class with methods

--- mainv2.py
+++ mainv2.py
@@ -70,3 +70,72 @@ def download_images(directory, image_links, image_names):
         save_as = os.path.join(directory, f'{image_names[counter]}.jpg')
         wget.download(link, save_as)
         counter += 1
+
+class ClothingItem:
+    def __init__(self, url):
+        self.url = url
+        self.name = None
+        self.price = None
+        self.id = None
+        self.image_links = []
+        self.image_names = []
+        self.directory = None
+    
+    def extract_data(self):
+        driver = webdriver.Chrome(executable_path='drivers\chromedriver.exe')
+        driver.maximize_window()
+        driver.get(self.url)
+        driver_wait = WebDriverWait(driver, 10)
+
+        # Determine the corresponding locator class based on the domain of the URL
+        domain = urlparse(self.url).hostname
+        if 'reserved.com' in domain:
+            locator_class = ReservedProductPageLocators
+        elif 'house.com' in domain:
+            locator_class = HouseProductPageLocators
+        elif 'sinsay.com' in domain:
+            locator_class = SinsayProductPageLocators
+        elif 'cropp.com' in domain:
+            locator_class = CroppProductPageLocators
+        elif 'mohito.com' in domain:
+            locator_class = MohitoProductPageLocators
+        else:
+            raise ValueError(f"No locator class found for domain '{domain}'")
+
+        # Extract data using the locators from the corresponding locator class
+        name_locator = getattr(locator_class, 'clothes_name_locator')
+        price_locator = getattr(locator_class, 'clothes_price_locator')
+        id_locator = getattr(locator_class, 'clothes_id_locator')
+        pic_locator = getattr(locator_class, 'clothes_pic_locator')
+
+        name = driver_wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, name_locator))).text
+        price = driver_wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, price_locator)))
+        item_id = driver_wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, id_locator))).text
+        clothes_pics = driver.find_elements(By.CSS_SELECTOR, pic_locator)
+        image_links = [clothes.get_attribute('src') for clothes in clothes_pics]
+        image_names = [clothes.get_attribute('alt') for clothes in clothes_pics]
+        image_links = prepare_link(image_links)
+
+        driver.quit()
+        
+        self.name = name
+        self.price = price
+        self.id = item_id
+        self.image_links = image_links
+        self.image_names = image_names
+
+    def create_directory(self):
+        path = os.getcwd()
+        self.directory = os.path.join(path, f'Reserved\{self.name}({self.id})')
+        os.mkdir(self.directory)
+
+    def download_images(self):
+        download_images(self.directory, self.image_links, self.image_names)
+        
+if __name__ == '__main__':
+    urls = input("Enter the URLs separated by commas: ").split(",")
+    for url in urls:
+        item = ClothingItem(url.strip())
+        item.extract_data()
+        item.create_directory()
+        item.download_images()              
